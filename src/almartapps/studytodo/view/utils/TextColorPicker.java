d2f3889a094a2464adcbd8e9d56794645ec84c28
David Martínez <gollumdeagol@gmail.com>
2014-03-08 19:19:43 +0100

    Added TextColorPicker utility method to dynamically change the text color compared to the background (using perceived luminance) to increase readability and accessibility.

--- src/almartapps/studytodo/view/utils/TextColorPicker.java
+++ src/almartapps/studytodo/view/utils/TextColorPicker.java
@@ -1,10 +1,29 @@
 package almartapps.studytodo.view.utils;
 
+import android.graphics.Color;
+
 
 public class TextColorPicker {
 
-	public static void pickTextColorFromBackground() {
-		//TODO calculate Text color taking into account the dark/bright nature of the background color
+	private static double THRESHOLD = 0.5;
+	
+	/**
+	 * Calculates the right text color given an arbitrary background
+	 * color. Text color will be either {@link Color.BLACK} or {@link Color.WHITE}.
+	 *  
+	 * @param backgroundColor the background color where the text will be displayed
+	 * @return the optimal text color for the given background
+	 */
+	public static int pickTextColorFromBackground(int backgroundColor) {
+		//gamma is the perceived luminance of the color
+		double gamma = 1.0 - (0.299 * ((double)Color.red(backgroundColor)) 
+							+ 0.587 * ((double)Color.green(backgroundColor)) 
+							+ 0.114 * ((double)Color.blue(backgroundColor))) / 255.0;
+		
+        if (gamma < THRESHOLD)
+        	return Color.BLACK;
+        else
+        	return Color.WHITE;
 	}
 	
 }
