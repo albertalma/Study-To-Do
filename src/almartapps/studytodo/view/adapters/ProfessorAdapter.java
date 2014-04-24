5ed3eb7280a61ecc48ac1e42cae22b858aaf5632
David Martínez <gollumdeagol@gmail.com>
2014-04-24 13:48:07 +0200

    styling issues

--- src/almartapps/studytodo/view/adapters/ProfessorAdapter.java
+++ src/almartapps/studytodo/view/adapters/ProfessorAdapter.java
@@ -1,20 +1,14 @@
 package almartapps.studytodo.view.adapters;
 
 import java.util.List;
-import java.util.Map;
 
 import almartapps.studytodo.R;
 import almartapps.studytodo.domain.model.Professor;
-import almartapps.studytodo.domain.model.Subject;
-import almartapps.studytodo.domain.model.Task;
 import android.content.Context;
-import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.LayerDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class ProfessorAdapter extends ArrayAdapter<Professor> {
